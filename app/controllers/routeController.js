fa9087f9c9c68977c2514474bbf81a3ea7a4530f
kennethlumalicay <kennethlumalicay@gmail.com>
2017-06-11 11:24:56 -0400

    just need to add validations

--- app/controllers/routeController.js
+++ app/controllers/routeController.js
@@ -63,29 +63,35 @@ function RouteController () {
 	this.votePoll = function(req, res, logged) {
 		Polls.findOne({ "poll.id" : req.params.id}, function(err, data) {
 			console.log("req.ip", req.ip, "req.ips", req.ips);
-			if(err) throw err;
-			else {
-				var choice = req.body.choice;
-				var newchoice = req.body.otherchoice;
-				if(choice=='other')
-					data.poll.choices.push({
-						choice: newchoice,
-						votes: 1
-					});
+			if(data.votes.includes(req.ip)) {
+				res.send({ anti_double_vote_mechanism: "sending an army of worms to user's address. please vote again in a few hours.",
+						good_guy_link_saver: "here is link to the poll you were viewing (" + process.env.APP_URL+"view/"+req.params.id+")."});
+			} else {
+				if(err) throw err;
 				else {
-					var index = -1;
-					data.poll.choices = data.poll.choices.map(e=>{
-						console.log(e.choice,newchoice);
-						if(e.choice==choice) {
-							e.votes = e.votes+1
-						} return e;
-					});
+					var choice = req.body.choice;
+					var newchoice = req.body.otherchoice;
+					if(choice=='other')
+						data.poll.choices.push({
+							choice: newchoice,
+							votes: 1
+						});
+					else {
+						var index = -1;
+						data.poll.choices = data.poll.choices.map(e=>{
+							console.log(e.choice,newchoice);
+							if(e.choice==choice) {
+								e.votes = e.votes+1
+							} return e;
+						});
+					}
+					data.votes.push(req.ip);
+					data.save(function(err, data) {
+						if(err) throw err;
+						else console.log(data.poll.title, "has been updated.")
+						res.redirect('/view/'+req.params.id);
+					})
 				}
-				data.save(function(err, data) {
-					if(err) throw err;
-					else console.log(data.poll.title, "has been updated.")
-					res.redirect('/view/'+req.params.id);
-				})
 			}
 		});
 	}
